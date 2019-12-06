bffa2bffcd85dd22947829434a9a394ed55646d9
nrico90 <nrico90@gmail.com>
2019-12-06 10:58:58 +0100

    create table

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
 const db = require("./db");
+const Image = require("../server/image/model");
 app.listen(port, () => console.log(`listening on ${port}`));
